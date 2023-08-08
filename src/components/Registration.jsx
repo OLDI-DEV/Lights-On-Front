c5062f6a774a2ec28b283493e7411ba2c8566fdb
gordeev-igor01~ <gordeev070814@gmail.com>
2023-08-08 18:12:06 +0300

    fixed media

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,8 +1,10 @@
 
 import styled from 'styled-components'
-import registrationBackgroundImg from '../assets/images/registrationImg.svg'
+import registrationBackgroundImg from '../assets/images/registrationImg.png'
 import registrationPhoneImg from '../assets/images/registration-phone.png'
 import registrationPhoneBgImg from '../assets/images/registration-phone-bg.png'
+import smileImg from '../assets/images/input-smile.svg'
+import emailImg from '../assets/images/input-email.svg'
 
 const Registration = () => {
   return (
@@ -15,33 +17,35 @@ const Registration = () => {
         <InputBlock>
           <InputLabel>Имя </InputLabel>
           <Input type='text' placeholder='Как мы можем к вам обращаться?'></Input>
-          <InputIcon />
+          <InputIcon
+            src={smileImg} />
         </InputBlock>
 
         <InputBlock>
           <InputLabel>Email </InputLabel>
           <Input type='text' placeholder='Адрес вашей электронной почты'></Input>
-          <InputIcon />
+          <InputIcon
+            src={emailImg} />
+        </InputBlock>
+        <InputBlock>
+          <CommentTextArea placeholder='Опишите, что вас интересует' />
+        </InputBlock>
+        <InputBlock>
+          <SubmitBtn>
+            Отправить заявку
+          </SubmitBtn>
         </InputBlock>
 
-        <CommentTextArea placeholder='Опишите, что вас интересует' />
-
-
-        <SubmitBtn>
-          Отправить заявку
-        </SubmitBtn>
       </RegistrationInner>
 
       <RegistrationImgBlock>
         <BackgroundImg
           src={registrationBackgroundImg}
         />
-        <PhoneImg
-          src={registrationPhoneImg}
-        />
-        <RegistrationBgPhoneImg
+
+        {/*         <RegistrationBgPhoneImg
           src={registrationPhoneBgImg}
-        />
+        /> */}
       </RegistrationImgBlock>
 
 
@@ -71,14 +75,17 @@ const PhoneImg = styled.img`
 position:absolute;
 left:105px;
 top:60px;
-  @media screen and (max-width: 650px) {
+/*   @media screen and (max-width: 650px) {
 display:none;
-  }
+  } */
 `
 
 const BackgroundImg = styled.img`
+width:100%;
+min-width:0;
+object-fit:contain;
   @media screen and (max-width: 650px) {
-display:none;
+/* display:none; */
   }
 `
 
@@ -102,6 +109,7 @@ border-radius: 88px;
 background: #34C759;
 padding:12px 24px;
 cursor:pointer;
+width:calc(100% + 16px);
 `
 
 const CommentTextArea = styled.textarea`
@@ -121,7 +129,7 @@ line-height: 24px;
 letter-spacing: -0.09px;
 min-height:90px;
 margin-bottom: 24px;
-width:calc(100% - 32px);
+width:calc(100% - 10px);
   @media screen and (max-width: 480px) {
   font-size: 10px;
   font-style: normal;
@@ -131,11 +139,14 @@ width:calc(100% - 32px);
 `
 
 const InputIcon = styled.img`
-
+position:absolute;
+top:40px;
+left:12px;
 `
 const Input = styled.input`
 width:calc(100% - 32px);
-padding:12px 16px;
+
+padding:12px 16px 12px 38px;
 border-radius: 6px;
 border: 1px solid  #E7EAEE;
 background:  #FFF;
@@ -180,7 +191,9 @@ letter-spacing: -0.09px;
 
 const InputBlock = styled.div`
 flex-direction:column;
-
+position:relative;
+display: flex;
+width:calc( 100% - 28px);
 `
 
 
@@ -206,11 +219,11 @@ line-height: normal;
 letter-spacing: -0.72px;
   }
     @media screen and (max-width: 480px) {
-font-size: 20px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-letter-spacing: -0.4px;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.4px;
   }
 
 `
@@ -232,8 +245,8 @@ const RegistrationContainer = styled.div`
   justify-content: space-between;
   align-items:center;
   padding:0 16px;
-  gap:15px;
-  @media screen and (max-width: 1000px) {
+  gap:50px;
+  @media screen and (max-width: 1100px) {
     gap:60px;
     flex-direction:column;
   }
